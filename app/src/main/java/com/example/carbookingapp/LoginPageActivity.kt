67bd0636883d1d97113dce9b3b1a106e0e175a08
Knighthawk213 <82392322+Knighthawk213@users.noreply.github.com>
2025-05-31 03:35:09 +0530

    Basic Login page

--- app/src/main/java/com/example/carbookingapp/LoginPageActivity.kt
+++ app/src/main/java/com/example/carbookingapp/LoginPageActivity.kt
@@ -1,20 +1,26 @@
 package com.example.carbookingapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.carbookingapp.databinding.ActivityLoginPageBinding
 
 class LoginPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login_page)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val binding = ActivityLoginPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.loginButton.setOnClickListener{
+            val username = binding.editUsername.text.toString()
+            val password = binding.editPassword.text.toString()
+            if(username.equals("Fateh") && password.equals("12345")){
+                Toast.makeText(this,"Success!",Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this,"Invalid!",Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
