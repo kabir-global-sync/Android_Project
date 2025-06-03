965478721b902c5b161b53e317a55a7465141ac0
kabir-global-sync <altamashkabir101@gmail.com>
2025-06-03 18:53:50 +0530

    password info alert added

--- app/src/main/java/com/example/carbookingapp/LoginPageActivity.kt
+++ app/src/main/java/com/example/carbookingapp/LoginPageActivity.kt
@@ -3,11 +3,8 @@ package com.example.carbookingapp
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.carbookingapp.databinding.ActivityLoginPageBinding
 
 class LoginPageActivity : AppCompatActivity() {
