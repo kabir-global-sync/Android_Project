64ebebfea1b4dca0b70d266f9169dbb409e1e2f1
kabir-global-sync <altamashkabir101@gmail.com>
2025-06-03 18:52:46 +0530

    password info alert added

--- app/src/main/java/com/example/carbookingapp/LoginPageActivity.kt
+++ app/src/main/java/com/example/carbookingapp/LoginPageActivity.kt
@@ -1,8 +1,10 @@
 package com.example.carbookingapp
 
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -16,11 +18,26 @@ class LoginPageActivity : AppCompatActivity() {
         binding.loginButton.setOnClickListener{
             val username = binding.editUsername.text.toString()
             val password = binding.editPassword.text.toString()
-            if(username.equals("Fateh") && password.equals("12345")){
+            if(username == "Fateh" && password == "12345"){
                 Toast.makeText(this,"Success!",Toast.LENGTH_LONG).show()
             }else{
                 Toast.makeText(this,"Invalid!",Toast.LENGTH_LONG).show()
             }
         }
+        val infoButton = findViewById<ImageButton>(R.id.infoalertid)
+        infoButton.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("Password Requirements")
+                .setMessage(
+                    "• At least 8 characters\n" +
+                            "• One uppercase letter\n" +
+                            "• One lowercase letter\n" +
+                            "• One number\n" +
+                            "• One special character (!@#\$%^&*)"
+                )
+                .setPositiveButton("OK", null)
+                .show()
+        }
+
     }
 }
