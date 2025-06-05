8ef95fa2916d02ecfdd078435cecc7f549e7eb22
kabir-global-sync <altamashkabir101@gmail.com>
2025-06-05 20:00:14 +0530

    splash screen added

--- app/src/main/java/com/example/carbookingapp/splashScreen.kt
+++ app/src/main/java/com/example/carbookingapp/splashScreen.kt
@@ -1,5 +1,6 @@
 package com.example.carbookingapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+@SuppressLint("CustomSplashScreen")
 class splashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
